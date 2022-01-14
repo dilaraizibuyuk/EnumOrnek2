d8b7a743cdf206039068ce170ac4dfe4d02907da
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-14 20:40:47 +0300

    Proje dosyası ekle.

---
+++ EnumOrnek2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumOrnek2
+{
+    class Program
+    {
+        public enum KonutTuru
+        {
+            Belirsiz,
+            Apart,
+            Apartman,
+            Residence,
+            Villa
+        }
+        public class Ev
+        {
+            public KonutTuru ozellik;
+
+            public Ev()
+            {
+                ozellik = KonutTuru.Belirsiz;
+            }
+
+            public void Goruntule()
+            {
+                Console.WriteLine("Konut Türü: {0}", ozellik);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Ev bicim = new Ev();
+            bicim.Goruntule();
+            bicim.ozellik = KonutTuru.Villa;
+            bicim.Goruntule();
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EnumOrnek2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("EnumOrnek2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EnumOrnek2")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("dc00eeab-d83a-4a56-8dff-df7e0806e6e2")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
